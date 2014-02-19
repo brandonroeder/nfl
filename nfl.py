e96252da5679ba600c48665af8cbe35c5245dbad
Brandon Roeder <brandonsroeder@gmail.com>
2014-02-18 19:09:32 -0600

    added turnovers

--- nfl.py
+++ nfl.py
@@ -29,15 +29,15 @@ game1= Game(homeTeam, awayTeam, 0,0,3,3)
 
 
 class Drive:
-	def __init__ (self, possession, number, playType, yardage, touchdown, penalty, timeout, turnover):
-		self.possession= possession
-		self.number= number
-		self.playType= playType
-		self.yardage= yardage
-		self.touchdown= touchdown
-		self.penatly= penalty
-		self.timeout= timeout
-		self.turnover=turnover
+	def __init__ (self, possession, player1, playType, receiver, yardage, touchdown,timeout, turnover):
+			self.possession= possession
+			self.player1= player1
+			self.playType= playType
+			self.receiver= receiver
+			self.yardage= yardage
+			self.touchdown= touchdown
+			self.timeout= timeout
+			self.turnover=turnover
 
 	def find(name, team=None):
 		players=nflgame.combine()
@@ -48,20 +48,34 @@ class Drive:
 					hits.append(player)
 					return hits
 
+def switchpossession(possession):
+  switchpos= possession
+  if switchpos==game1.home:
+    switchpos= game1.away
+    return switchpos
+  else:
+    switchpos=game1.home
+    return switchpos
 
-first= Drive(game1.home, "case keenum", "pass", 34, 0, 0,1,0)
 
-player1=nflgame.find(first.number)
+first= Drive(game1.home, "case keenum","pass", "andre johnson", 34, 0,0,1)
+
+player1=nflgame.find(first.player1)
 for p in player1:
 	player=p
+
+receiver=nflgame.find(first.receiver)
+for p in receiver:
+	receiver=p
 
 
 if first.playType=="pass":
 	type=" passed"
+	print str(player) +type + " the ball to " + str(receiver)+ " for " +str(first.yardage) + " yards."
 else:
 	type=" ran"
+	print str(player)+type+ " the ball for "+ str(first.yardage)+ " yards."
 
-print str(player) +type + " the ball for " +str(first.yardage) + " yards."
 
 if first.touchdown==1:
 	game1.homeScore+=7
@@ -70,14 +84,15 @@ if first.touchdown==1:
 	print game1.away + ": " +str(game1.awayScore)
 
 if first.turnover==1:
-	print "Turnover!"
+  print "Turnover!"
+  first.possession=switchpossession(first.possession)
 
 if first.timeout==1:
 	game1.homeTimeout=game1.homeTimeout-1
 	print game1.home + " has called a timeout. They have " + str(game1.homeTimeout)+ " timeouts left."
 
-print str(first.possession) +" has the ball."
 
+print str(first.possession) +" has the ball."
 
 
 
